16c48ed8d4417e1a39241c4fae4908ff23145b83
tomib <tomibottoni@gmail.com>
2023-09-09 20:02:41 -0300

    Carpetas y modulos iniciales

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ], 
+  exports:[] //Acá van todos los componentes para compartirlos a los demás modulos
+})
+export class SharedModule { }
