c5dc7f0a6d61a3c66a4fad5038736dfd1f242c18
octapereyra <octaviopereyra19@gmail.com>
2023-09-12 21:15:20 -0300

    added user logged design

--- src/app/modules/shared/navbar/navbar.component.ts
+++ src/app/modules/shared/navbar/navbar.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  @Input() showSearch!: boolean;
+  @Input() user!: any; //como recibo el usuario que está logeado??
+
+  constructor() {
+    this.user = 'Pepito';
+  }
+}
