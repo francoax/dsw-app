aba0f2a767aed79ad252192b2bdef00bee898064
francoax <francoa.duarte2001@gmail.com>
2023-09-18 22:34:59 -0300

    declared input for list of buttons.

--- src/app/modules/shared/sidebar/sidebar.component.ts
+++ src/app/modules/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+type ButtonsForSideBar = {
+  name : string,
+  path : string
+}[]
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -7,57 +11,10 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit{
 
-  superAdminRoutes = [
-    {
-      name : 'Agregar Administrador',
-      path : ''
-    },
-    {
-      name : 'Lista de Administradores',
-      path : ''
-    }
-  ]
-
-  adminRoutes = [
-    {
-      name : 'Autos',
-      path : ''
-    },
-    {
-      name : 'Propiedades',
-      path : ''
-    },
-    {
-      name : 'Tipo de propiedades',
-      path : ''
-    },
-    {
-      name : 'Localidades',
-      path : ''
-    },
-    {
-      name : 'Asistencia Medica',
-      path : ''
-    },
-    {
-      name : 'Paquetes',
-      path : ''
-    }
-  ]
-
-  buttonsToShow = [{ name : '', path : ''}];
-
-  userRole! : string;
+  @Input() buttonsList! : ButtonsForSideBar;
+  buttonsToShow : ButtonsForSideBar = [];
 
   ngOnInit(): void {
-    if(this.userRole === 'SUPER_ADMIN') {
-      this.buttonsToShow = [...this.superAdminRoutes]
-    }
-
-    if(this.userRole === 'ADMIN') {
-      this.buttonsToShow = [...this.adminRoutes]
-    }
-
-    this.buttonsToShow = [...this.adminRoutes]
+    this.buttonsToShow = [...this.buttonsList];
   }
 }
