82034aa31c7e082b9bbcd51faf4c4b2b8bc885a8
NicolasDiDio09 <NicolasDiDio09@gmail.com>
2023-09-25 18:16:34 -0300

    begin abmc components cars

---
+++ src/app/models/car.ts
@@ -0,0 +1,13 @@
+export interface Car {
+  brand: string
+  model: string
+  year: string
+  plate: string
+  price: Price
+  locality: string
+}
+export interface Price {
+  date: string
+  value: string
+}
+
