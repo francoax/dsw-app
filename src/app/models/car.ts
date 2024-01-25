5a40123171518fe9efda5195bdfc075b97a04d3b
octapereyra <octaviopereyra19@gmail.com>
2024-01-25 16:29:27 -0300

    add api's for location, cars form modified, car model changed

--- src/app/models/car.ts
+++ src/app/models/car.ts
@@ -1,17 +1,13 @@
 export interface Car {
-  id: string,
-  brand: string,
-  model: string,
-  year: string,
-  plate: string,
-  price: Price,
-  locality:{
-    id : string,
-    name: string;
-  };
+  id: string;
+  brand: string;
+  model: string;
+  year: string;
+  plate: string;
+  price: Price;
+  locality: string;
 }
 export interface Price {
-  date: string,
-  value: number
+  date: string;
+  value: number;
 }
-
