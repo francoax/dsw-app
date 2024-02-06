48ecb951f74bd8427ba8aec6b465b1d6aef0e05c
NicolasDiDio09 <NicolasDiDio09@gmail.com>
2024-02-06 20:11:19 -0300

    authguard & sidebar & nav fixed

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,20 +1,31 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
+
+interface JwtPayload {
+  userId: string;
+  name: string;
+  email: string;
+  role: string;
+  iat: number;
+}
 
 export const authGuard: CanActivateFn = (route, state) => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const loggedUser = JSON.parse(localStorage.getItem('loggedUser')!)
+  
+  const loggedUser = localStorage.getItem('loggedUser');
+  const decodedToken: JwtPayload = jwtDecode(loggedUser!);
+  console.log(decodedToken);
   const router = inject(Router)
   if(!loggedUser) {
     router.navigate(['/login'])
     return false
   }
   const expectedRole = route.data['expectedRole'];
-  const userRole = loggedUser.role
+  const userRole = decodedToken.role;
   if (userRole && userRole === expectedRole) {
     return true;
   }
-
   router.navigate(['/unauthorized']);
   return false;
 };
