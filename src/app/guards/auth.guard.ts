b208a58e824aa62ff3bf2ac054035baad69c9a13
francoax <francoa.duarte2001@gmail.com>
2024-02-07 14:05:49 -0300

    guard fixed.

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -11,21 +11,23 @@ interface JwtPayload {
 }
 
 export const authGuard: CanActivateFn = (route, state) => {
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  
   const loggedUser = localStorage.getItem('loggedUser');
-  const decodedToken: JwtPayload = jwtDecode(loggedUser!);
-  console.log(decodedToken);
-  const router = inject(Router)
-  if(!loggedUser) {
-    router.navigate(['/login'])
-    return false
+  const router = inject(Router);
+
+  if (!loggedUser) {
+    router.navigate(['/login']);
+    return false;
   }
+
   const expectedRole = route.data['expectedRole'];
+
+  const decodedToken: JwtPayload = jwtDecode(loggedUser);
   const userRole = decodedToken.role;
+
   if (userRole && userRole === expectedRole) {
     return true;
   }
+
   router.navigate(['/unauthorized']);
   return false;
 };
